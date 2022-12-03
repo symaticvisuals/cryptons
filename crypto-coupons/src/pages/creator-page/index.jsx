f17d42cf4bf0a2ed856fbed63ce213384b9de21c
Deepanshu Goel <symaticvisuals02@gmail.com>
2022-12-04 01:55:38 +0530

    conflict fix

--- crypto-coupons/src/pages/creator-page/index.jsx
+++ crypto-coupons/src/pages/creator-page/index.jsx
@@ -7,15 +7,101 @@ import "@biconomy/web3-auth/dist/src/style.css"
 import { ColorConstants } from "../../ColorConstants";
 
 function CreatorPage() {
-  const [checked, setChecked] = useState(false);
-  const [formData, setFormData] = useState({
-    amount: 0,
-    number_of_coupons: 0,
-    expiry_date: "",
-    keywords: "CRYPTONS",
-  });
+  const [data, setData] = useState("No result");
+  // const [address, setAddress] = useState(null);
+  const initialState = {
+    provider: null,
+    web3Provider: null,
+    ethersProvider: null,
+    address: "",
+    chainId: "80001",
+  };
+  const [web3State, setWeb3State] = useState(initialState);
+  const { provider, web3Provider, ethersProvider, address, chainId } =
+    web3State;
+  const [loading, setLoading] = useState(false);
+  const [socialLoginSDK, setSocialLoginSDK] = useState(null);
+  const [userInfo, setUserInfo] = useState(null);
 
-  const [address, setAddress] = useState("");
+  useEffect(() => {
+    console.log("hidelwallet");
+    if (socialLoginSDK && socialLoginSDK.provider) {
+      socialLoginSDK.hideWallet();
+    }
+  }, [address, socialLoginSDK]);
+  const [gSigner, setGSigner] = useState(null);
+  const connect = useCallback(async () => {
+    if (address) return;
+    if (socialLoginSDK?.provider) {
+      setLoading(true);
+      console.log(socialLoginSDK.provider, "+++++++++++++=");
+      console.info("socialLoginSDK.provider", socialLoginSDK.provider);
+      const web3Provider = new ethers.providers.Web3Provider(
+        socialLoginSDK.provider
+      );
+      const signer = web3Provider.getSigner();
+      setGSigner(signer);
+      const gotAccount = await signer.getAddress();
+      const network = await web3Provider.getNetwork();
+      setWeb3State({
+        provider: socialLoginSDK.provider,
+        web3Provider: web3Provider,
+        ethersProvider: web3Provider,
+        address: gotAccount,
+        chainId: Number(network.chainId),
+      });
+      setLoading(false);
+      return;
+    }
+    if (socialLoginSDK) {
+      socialLoginSDK.showWallet();
+      return socialLoginSDK;
+    }
+    setLoading(true);
+    const sdk = await getSocialLoginSDK("0x13881");
+    sdk.showConnectModal();
+    sdk.showWallet();
+    setSocialLoginSDK(sdk);
+    setLoading(false);
+    return socialLoginSDK;
+  }, [address, socialLoginSDK]);
+
+  const getUserInfo = useCallback(async () => {
+    if (socialLoginSDK) {
+      const userInfo = await socialLoginSDK.getUserInfo();
+      console.log("userInfo", userInfo);
+      setUserInfo(userInfo);
+    }
+  }, [socialLoginSDK]);
+
+  // after metamask login -> get provider event
+  useEffect(() => {
+    const interval = setInterval(async () => {
+      if (address) {
+        clearInterval(interval);
+      }
+      if (socialLoginSDK?.provider && !address) {
+        connect();
+      }
+    }, 1000);
+    return () => {
+      clearInterval(interval);
+    };
+  }, [address, connect, socialLoginSDK]);
+
+  const callConnectWallet = async () => {
+    // sendNotification("0x125a287746989EABeb71c795c5114E311C4D02f7")
+    connect();
+
+    // const res = await connectBWallet();
+    // console.log(JSON.stringify(res))
+    // setAddress(res)
+  };
+
+  const test = async () => {
+    console.log("testt", address, gSigner, chainId);
+    optInSubscription(address, gSigner);
+  };
 
   return (
     <div className="flex justify-center mt-[4vh]  h-full overflow-y-auto">
